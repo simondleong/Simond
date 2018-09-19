43afd75da8b4c716aa50477a30e9d610b2e719cf
simondleong <ezio.leong@hotmail.com>
2018-09-20 03:23:39 +0700

    Update controller and add PayPal PHP SDK dependency

--- app/Http/Controllers/DateController.php
+++ app/Http/Controllers/DateController.php
@@ -16,7 +16,7 @@ class DateController extends Controller
         $this->user = $user;
         $this->date = $date;
 
-        $this->config         = Config::get('constants');
+        $this->config   = Config::get('constants');
         $this->gender   = $this->config['gender'];
         $this->date_status  = $this->config['date_status'];
         $this->city = $this->config['city'];
@@ -40,6 +40,7 @@ class DateController extends Controller
         $date->sender_id = $user->id;
         $date->receiver_id = $receiver;
         $date->status = 0;
+        $date->payment_status = 0;
         $date->sender_confirmation = 0;
         $date->receiver_confirmation = 0;
 
@@ -58,8 +59,8 @@ class DateController extends Controller
         $dates  = $this->date->where('sender_id', $user->id)->orderBy('created_at', 'desc')->paginate(10);
 
         return view('sent')->with('dates', $dates)
-                                ->with('gender', $this->gender)
-                                ->with('status', $this->date_status);
+            ->with('gender', $this->gender)
+            ->with('status', $this->date_status);
     }
 
 
@@ -71,8 +72,8 @@ class DateController extends Controller
         $dates  = $this->date->where('receiver_id', $user->id)->orderBy('created_at', 'desc')->paginate(10);
 
         return view('received')->with('dates', $dates)
-                                    ->with('gender', $this->gender)
-                                    ->with('status', $this->date_status);
+            ->with('gender', $this->gender)
+            ->with('status', $this->date_status);
     }
 
 
@@ -91,16 +92,16 @@ class DateController extends Controller
         }
 
         return view('date')->with('date', $date)
-                                ->with('user', $user)
-                                ->with('target', $target)
-                                ->with('data', [
-                                    'config' => $this->config,
-                                    'city' => $this->city,
-                                    'gender' => $this->gender,
-                                    'sexual_preference' => $this->sexual_preference,
-                                    'personality_type' => $this->personality_type,
-                                    'age' => $this->age
-                                ]);
+            ->with('user', $user)
+            ->with('target', $target)
+            ->with('data', [
+                'config' => $this->config,
+                'city' => $this->city,
+                'gender' => $this->gender,
+                'sexual_preference' => $this->sexual_preference,
+                'personality_type' => $this->personality_type,
+                'age' => $this->age
+            ]);
     }
 
 
